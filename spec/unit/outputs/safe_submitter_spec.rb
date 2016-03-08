ebb78a421296495f67ff61ab4b1ded92918c1a68
Elad Amit <elad@perimeterx.com>
2016-03-08 18:09:16 +0200

    added empty spec for SafeSubmitter

---
+++ spec/unit/outputs/safe_submitter_spec.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+require_relative "../../cassandra_spec_helper"
+require "logstash/outputs/cassandra/safe_submitter"
+
+RSpec.describe LogStash::Outputs::Cassandra::SafeSubmitter do
+
+end
