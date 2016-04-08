30edb4768f98c6c5e63695a6cd457510b24c47dd
Elad Amit <elad@perimeterx.com>
2016-04-08 16:39:53 +0300

    extracted shared examples

--- spec/unit/outputs/backoff_retry_policy_spec.rb
+++ spec/unit/outputs/backoff_retry_policy_spec.rb
@@ -2,34 +2,35 @@
 require_relative "../../cassandra_spec_helper"
 require "logstash/outputs/cassandra/backoff_retry_policy"
 
+RSpec.shared_examples "limited parameterized backoff" do
+  let(:sut) { ::Cassandra::Retry::Policies::Backoff }
+
+  it "stops once the max retries are reached"
+  it "waits between retries"
+  it "allows for exponential backoffs"
+  it "allows for linear backoffs"
+end
+
 RSpec.describe ::Cassandra::Retry::Policies::Backoff do
   let(:sut) { ::Cassandra::Retry::Policies::Backoff }
 
   describe "#read_timeout" do
-    it "tries again if the result did not finish, but the required acks were"
-    it "tries the next host, if retries are left"
+    include_examples "limited parameterized backoff"
 
-    it "stops once the max retries are reached"
-    it "waits between retries"
+    it "tries again if the result did not arrive, but the required acks arrived"
+    it "tries the next host, if retries are left"
   end
 
   describe "#write_timeout" do
+    include_examples "limited parameterized backoff"
+
     it "tries the next host if no acks were recieved (there is an undelying assumption that the query is idempotent)"
     it "retries if the query was a logged batch"
-
-    it "stops once the max retries are reached"
-    it "waits between retries"
   end
 
   describe "#unavailable" do
+    include_examples "limited parameterized backoff"
+
     it "tries the next host"
-
-    it "stops once the max retries are reached"
-    it "waits between retries"
-  end
-
-  describe "#calculate_backoff_wait_time" do
-    it "allows for exponential backoffs"
-    it "allows for linear backoffs"
   end
 end
