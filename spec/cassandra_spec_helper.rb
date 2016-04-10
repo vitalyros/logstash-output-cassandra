30ece3ef57be864f2b1963044b27d26f13f1e657
Elad Amit <elad@perimeterx.com>
2016-04-10 18:02:50 +0300

    adding metric fu, built in coverage calcs, and sonar

--- spec/cassandra_spec_helper.rb
+++ spec/cassandra_spec_helper.rb
@@ -1,3 +1,14 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/event"
+require 'simplecov'
+require 'simplecov-rcov'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::RcovFormatter
+])
+
+SimpleCov.start do
+  add_filter '/spec/'
+end
